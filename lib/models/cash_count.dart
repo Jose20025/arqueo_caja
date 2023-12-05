e2757804cdc17270d7378243d0e77c8890ac6bef
Jose Carlos <joseca20025@gmail.com>
2023-12-05 17:44:33 -0400

    edited model cashcount

--- lib/models/cash_count.dart
+++ lib/models/cash_count.dart
@@ -1,15 +1,22 @@
 class CashCount {
-  final double amount;
+  final double initalAmount;
+  double finalAmount = 0;
   final DateTime date;
 
   CashCount({
-    required this.amount,
+    required this.initalAmount,
     required this.date,
+    this.finalAmount = 0,
   });
+
+  void setFinalAmount(double finalAmount) {
+    this.finalAmount = finalAmount;
+  }
 
   factory CashCount.fromJson(Map<String, dynamic> json) {
     return CashCount(
-      amount: json['amount'],
+      initalAmount: json['initalAmount'],
+      finalAmount: json['finalAmount'],
       date: DateTime.parse(json['date']),
     );
   }
