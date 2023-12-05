07a819cd72cc1752c17649d9c5ae7717a8506931
Jose Carlos <joseca20025@gmail.com>
2023-12-05 14:22:18 -0400

    added custominputfield

---
+++ lib/custom/custom_input_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+  final String label;
+  final double? width;
+  final void Function(String?)? onSaved;
+  const CustomInputField(
+      {super.key, required this.label, required this.width, this.onSaved});
+
+  InputDecoration inputStyle(String label) {
+    return InputDecoration(
+      label: Text(label),
+      border: const OutlineInputBorder(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: TextFormField(
+        decoration: inputStyle(label),
+        keyboardType: TextInputType.number,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Este campo es requerido';
+          }
+          return null;
+        },
+        onSaved: onSaved,
+      ),
+    );
+  }
+}
