1f6d990cf948bfe0a1860d1f407d9cf3dfec2a07
Jose Carlos <joseca20025@gmail.com>
2023-12-05 17:44:08 -0400

    changed cashcountcard

--- lib/custom/cashcount_card.dart
+++ lib/custom/cashcount_card.dart
@@ -3,21 +3,79 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class CashCountCard extends StatelessWidget {
-  final CashCount cashCount;
+  const CashCountCard({
+    super.key,
+    required this.cashCount,
+  });
 
-  const CashCountCard({super.key, required this.cashCount});
+  final CashCount? cashCount;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: const Icon(Icons.attach_money),
-        title: Text(
-          NumberFormat.currency().format(cashCount.amount),
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
         ),
-        subtitle: Text(DateFormat.yMMMMd().format(cashCount.date)),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: SizedBox(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text('Plata en caja'),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Inicio',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Fin',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        NumberFormat.currency().format(cashCount!.initalAmount),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        NumberFormat.currency().format(cashCount!.finalAmount),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const ListTile(
+                    leading: Icon(Icons.date_range),
+                    title: Text('Fecha'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, bottom: 15),
+                    child: Text(
+                      DateFormat.yMEd().format(cashCount!.date),
+                      style: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
