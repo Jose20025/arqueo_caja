7d8a16137d8d1841f0c5fae463f1f5d3802b8437
Jose Carlos <joseca20025@gmail.com>
2023-12-04 23:48:16 -0400

    intl implementation in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:arqueo_caja/config/theme.dart';
 import 'package:arqueo_caja/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/number_symbols.dart';
+import 'package:intl/number_symbols_data.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  Intl.defaultLocale = 'es_BO';
+  initializeDateFormatting('es_BO', null);
+
+  numberFormatSymbols['es_BO'] = const NumberSymbols(
+    NAME: 'es_BO',
+    DECIMAL_SEP: ',',
+    GROUP_SEP: '.',
+    PERCENT: '%',
+    ZERO_DIGIT: '0',
+    PLUS_SIGN: '+',
+    MINUS_SIGN: '-',
+    EXP_SYMBOL: 'E',
+    PERMILL: '‰',
+    INFINITY: '∞',
+    NAN: 'NaN',
+    DECIMAL_PATTERN: '#,##0.###',
+    SCIENTIFIC_PATTERN: '#E0',
+    PERCENT_PATTERN: '#,##0%',
+    CURRENCY_PATTERN: '¤#,##0.00',
+    DEF_CURRENCY_CODE: 'Bs ',
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
