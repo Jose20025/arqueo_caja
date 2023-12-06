ee03cc51c70b6f9f921fb7821ffd7a93e91cefaa
Jose Carlos <joseca20025@gmail.com>
2023-12-05 22:07:13 -0400

    implemented id in model cashcout

--- lib/models/cash_count.dart
+++ lib/models/cash_count.dart
@@ -1,9 +1,11 @@
 class CashCount {
+  final String id;
   final double initalAmount;
   double finalAmount = 0;
   final DateTime date;
 
   CashCount({
+    required this.id,
     required this.initalAmount,
     required this.date,
     this.finalAmount = 0,
@@ -15,6 +17,7 @@ class CashCount {
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'initalAmount': initalAmount,
       'finalAmount': finalAmount,
       'date': date.toIso8601String(),
@@ -23,6 +26,7 @@ class CashCount {
 
   factory CashCount.fromJson(Map<String, dynamic> json) {
     return CashCount(
+      id: json['id'],
       initalAmount: json['initalAmount'],
       finalAmount: json['finalAmount'],
       date: DateTime.parse(json['date']),
