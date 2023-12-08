0a3ac7ea732448f0e20593a9e074752455518375
Jose Carlos <joseca20025@gmail.com>
2023-12-08 01:44:07 -0400

    design changes and implemented button to close cashcount

--- lib/custom/cashcount_card.dart
+++ lib/custom/cashcount_card.dart
@@ -13,87 +13,90 @@ class CashCountCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: cashCount.finalAmount == 0
-          ? () {
-              Navigator.pushNamed(context, '/add',
-                  arguments: Props(cashCount: cashCount, where: '/complete'));
-            }
-          : () {
-              ScaffoldMessenger.of(context).removeCurrentSnackBar();
-
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text(
-                    'Este arqueo ya fue finalizado, no se puede editar',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  backgroundColor: Colors.red,
-                  duration: Duration(milliseconds: 1500),
-                ),
-              );
-            },
+    return SizedBox(
       child: Column(
         children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: SizedBox(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const ListTile(
-                      leading: Icon(Icons.attach_money),
-                      title: Text('Plata en caja'),
-                    ),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+          Card(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text('Plata en caja'),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Inicio',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Fin',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        NumberFormat.currency().format(cashCount.initalAmount),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        NumberFormat.currency().format(cashCount.finalAmount),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const ListTile(
+                    leading: Icon(Icons.date_range),
+                    title: Text('Fecha'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          'Inicio',
-                          style: TextStyle(fontSize: 18),
+                          DateFormat.yMEd().format(cashCount.date),
+                          style: const TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w400),
                         ),
-                        Text(
-                          'Fin',
-                          style: TextStyle(fontSize: 18),
+                        FilledButton.tonal(
+                          onPressed: cashCount.finalAmount == 0
+                              ? () {
+                                  Navigator.pushNamed(context, '/add',
+                                      arguments: Props(
+                                          cashCount: cashCount,
+                                          where: '/complete'));
+                                }
+                              : null,
+                          style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(1),
+                          ),
+                          child: const Text('Cerrar arqueo'),
                         )
                       ],
                     ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          NumberFormat.currency()
-                              .format(cashCount.initalAmount),
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          NumberFormat.currency().format(cashCount.finalAmount),
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                  ),
+                  const SizedBox(height: 10),
+                  FilledButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Ver detalles',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    const ListTile(
-                      leading: Icon(Icons.date_range),
-                      title: Text('Fecha'),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, bottom: 15),
-                      child: Text(
-                        DateFormat.yMEd().format(cashCount.date),
-                        style: const TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
               ),
             ),
           ),
