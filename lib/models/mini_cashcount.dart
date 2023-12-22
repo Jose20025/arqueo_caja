30d50ba4cc0818c2a3a97ebd2ff282e101a9c60b
Jose Carlos <joseca20025@gmail.com>
2023-12-22 12:19:33 -0400

    added model mini_cashcount

---
+++ lib/models/mini_cashcount.dart
@@ -0,0 +1,12 @@
+class MiniCashCount {
+  final String id;
+  final String name;
+  final int amount;
+  final double total;
+
+  MiniCashCount(
+      {required this.id,
+      required this.name,
+      required this.amount,
+      required this.total});
+}
